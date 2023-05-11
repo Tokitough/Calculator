9414f658a2d812f00baecaabf0eb730e0f65b0f8
Kaiser Sajo <jhonkaisersajo@gmail.com>
2023-05-11 18:58:54 +0800

    Final

--- calculator.py
+++ calculator.py
@@ -1,17 +1,22 @@
 def calculator():
     try:
         # Ask user to choose an operation
-        print("Please choose an operation -\n"
+        print("=" * 45)
+        print("Please choose an operation: \n"
               "1. Addition\n"
               "2. Subtraction\n"
               "3. Multiplication\n"
-              "4. Division\n")
+              "4. Division")
+        print("=" * 45)
+        
         # Get user input for operation
         operation = int(input("Choose one of the four math operations (1-4): "))
 
         # Get user input for two numbers
+        print("=" * 45)
         num_1 = float(input("Enter the first number: "))
         num_2 = float(input("Enter the second number: "))
+        print("=" * 45)
         
         # Perform operation based on user input
         if operation == 1:
@@ -47,6 +52,7 @@ def calculator():
         calculator()
     except ValueError:
         # Handle invalid input error
+        print("=" * 45)
         print("Error: Invalid input.")
         calculator()
 
