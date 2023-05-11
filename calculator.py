343a7ac56090f44314acd1c74a755aec238cf8ca
Kaiser Sajo <jhonkaisersajo@gmail.com>
2023-05-11 17:21:20 +0800

    get user input

--- calculator.py
+++ calculator.py
@@ -6,3 +6,9 @@ def calculator():
               "2. Subtraction\n"
               "3. Multiplication\n"
               "4. Division\n")
+        # Get user input for operation
+        operation = int(input("Choose one of the four math operations (1-4): "))
+
+        # Get user input for two numbers
+        num_1 = float(input("Enter the first number: "))
+        num_2 = float(input("Enter the second number: "))
