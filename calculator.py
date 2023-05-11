519bed08a3e7b97d2c0e7504b74851c218ebfc86
Kaiser Sajo <jhonkaisersajo@gmail.com>
2023-05-11 17:03:21 +0800

    promt user

---
+++ calculator.py
@@ -0,0 +1,8 @@
+def calculator():
+    try:
+        # Ask user to choose an operation
+        print("Please choose an operation -\n"
+              "1. Addition\n"
+              "2. Subtraction\n"
+              "3. Multiplication\n"
+              "4. Division\n")
